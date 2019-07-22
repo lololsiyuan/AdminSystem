3524dcae7fbcd38c1fcfa08b9cf22bc0a3355c3a
lisiyuan <lisiyuan@papayamobile.cn>
2019-07-22 16:48:34 +0800

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,10 @@ module.exports = {
     /*
      ** Customize the progress-bar color
      */
-    loading: { color: '#fff' },
+    loading: { color: '#1ad' },
+    // router: {
+    //     base: '/AdminSystem/'
+    // }
     /*
      ** Global CSS
      */
